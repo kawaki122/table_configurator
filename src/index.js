ab8674f38a1ad9800a6bf71dec4fbea55eebad83
kawaki122 <kawaki122@gmail.com>
2023-08-10 12:05:12 -0700

    adjustments

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ function initialize() {
 
   renderer.setSize(container.clientWidth, container.clientHeight);
   container.appendChild(renderer.domElement);
-  camera.position.z = 200;
-  camera.position.y = 100;
+  camera.position.z = 20;
+  camera.position.y = 10;
 }
 
 function animate() {
@@ -57,23 +57,23 @@ function attachListeners() {
     frame.children[6].scale.setZ(1+(this.value/20));
     frame.children[7].scale.setZ(1+(this.value/20));
 
-    frame.children[3].position.setZ(-(this.value*30.8))
+    frame.children[3].position.setZ((this.value*30.8))
     frame.children[1].position.setZ(-(this.value*30.8))
-    frame.children[5].position.setZ(-(this.value*30.8))
+    frame.children[5].position.setZ((this.value*30.8))
 
-    frame.children[0].position.setZ((this.value*30.8))
-    frame.children[2].position.setZ((this.value*30.8))
+    frame.children[0].position.setZ(-(this.value*30.8))
+    frame.children[2].position.setZ(-(this.value*30.8))
     frame.children[4].position.setZ((this.value*30.8))
     lengthLabel.innerHTML = 20+Number(this.value)
   }
 
   width.oninput = function () {
-    frame.children[4].scale.setX(1+(this.value/20))
-    frame.children[5].scale.setX(1+(this.value/20))
+    frame.children[0].scale.setX(1+(this.value/20))
+    frame.children[3].scale.setX(1+(this.value/20))
 
-    frame.children[3].position.setX((this.value*11.1))
+    frame.children[5].position.setX((this.value*11.1))
     frame.children[1].position.setX(-(this.value*11.1))
-    frame.children[0].position.setX(-(this.value*11.1))
+    frame.children[4].position.setX(-(this.value*11.1))
     frame.children[2].position.setX((this.value*11.1))
 
     frame.children[6].position.setX(-(this.value*11.1))
@@ -83,12 +83,12 @@ function attachListeners() {
 
   height.oninput = function () {
     frame.children[1].scale.setY(1+(this.value/20))
-    frame.children[3].scale.setY(1+(this.value/20))
-    frame.children[0].scale.setY(1+(this.value/20))
     frame.children[2].scale.setY(1+(this.value/20))
+    frame.children[4].scale.setY(1+(this.value/20))
+    frame.children[5].scale.setY(1+(this.value/20))
 
-    frame.children[4].position.setY((this.value*36.45))
-    frame.children[5].position.setY((this.value*36.45))
+    frame.children[0].position.setY((this.value*36.45))
+    frame.children[3].position.setY((this.value*36.45))
     frame.children[6].position.setY((this.value*36.45))
     frame.children[7].position.setY((this.value*36.45))
     heightLabel.innerHTML = 10+Number(this.value)
@@ -110,8 +110,8 @@ function loadModel() {
   const loader = new OBJLoader(loadManager);
   loader.load(`./assets/models/thusia.obj`, (obj) => {
     frame = obj;
-    obj.scale.set(0.1, 0.1, 0.1)
-    
+    obj.scale.set(0.01, 0.01, 0.01)
+    console.log(obj)
     scene.add(obj);
   });
 }
